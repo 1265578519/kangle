a4c9ae1a88db54ef44c027cbdb27f583f8ce905e
小樱 <1265578519@qq.com>
2015-08-15 14:12:15 +0800

    更新ent版本

---
+++ ent/e.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+VERSION="3.4.8"
+if test $# != 1;then
+        echo "Usage: $0 dir"
+        exit 1;
+fi
+PREFIX=$1
+ARCH="-6"
+if test `ldd --version|head -1|awk '{print $NF;}'` = "2.5" ; then
+        ARCH="-5"
+fi
+if test `arch` = "x86_64"; then
+        ARCH="$ARCH-x64"
+fi
+URL="https://raw.githubusercontent.com/1265578519/kangle/master/ent/kangle-ent-$VERSION$ARCH.tar.gz"
+wget $URL -O kangle.tar.gz
+tar xzf kangle.tar.gz
+cd kangle
+./install.sh $PREFIX
