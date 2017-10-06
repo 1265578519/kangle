5c3e6ad7ff9c04e9377377503b0eabe15efaeb9e
小樱 <1265578519@qq.com>
2017-10-07 01:19:30 +0800

    推送ep2.6.22，修复删除空间后FTP还能登陆的问题
    推送ep2.6.22，修复删除空间后FTP还能登陆的问题

--- kangle/easypanel/ep.sh
+++ kangle/easypanel/ep.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 KANGLE_VERSION="3.5.11"
 PHP_VERSION="5.2.17"
-EASYPANEL_VERSION="2.6.20"
+EASYPANEL_VERSION="2.6.22"
 PUREFTP_VERSION="1.0.36"
 PREFIX="/vhs/kangle"
 CONFIG_FILES="/ext/tpl_php52/php-templete.ini"
