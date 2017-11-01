484c1d9721b1a3fd728ade5e56830b17e6d51436
小樱 <1265578519@qq.com>
2017-11-02 02:51:56 +0800

    更新所有PHP版本预编译版本
    更新所有PHP版本预编译版本

---
+++ php/5.5/5538/completed/tpl_php5538.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum -y install bzip2-devel libxml2-devel curl-devel db4-devel libjpeg-devel libpng-devel freetype-devel pcre-devel zlib-devel sqlite-devel libmcrypt-devel unzip bzip2
+yum -y install mhash-devel openssl-devel
+yum -y install libtool-ltdl libtool-ltdl-devel
+PREFIX="/vhs/kangle/ext"
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/5.5/5538/completed/tpl_php5538.tar.gz -O tpl_php5538.tar.gz
+tar xzf tpl_php5538.tar.gz
+mv tpl_php5538 $PREFIX
+rm -rf /tmp/*
+/vhs/kangle/bin/kangle -r
