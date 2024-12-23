233166fbc08fe05e63c394b521990c2eaeef6d96
小樱 <1265578519@qq.com>
2024-12-24 02:15:39 +0800

    极速安装调用仓库路径不正确（不影响安装）
    极速安装调用仓库路径不正确（不影响安装）

--- php/7.3/7333/completed/tpl_php7333.sh
+++ php/7.3/7333/completed/tpl_php7333.sh
@@ -3,7 +3,7 @@ yum -y install bzip2-devel libxml2-devel curl-devel db4-devel libjpeg-devel libp
 yum -y install mhash-devel openssl-devel
 yum -y install libtool-ltdl libtool-ltdl-devel
 yum -y remove libzip-devel
-wget -c http://github.itzmx.com/1265578519/kangle/master/php/7.3/730/libzip-1.3.2.tar.gz -O libzip-1.3.2.tar.gz
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/7.3/7333/libzip-1.3.2.tar.gz -O libzip-1.3.2.tar.gz
 tar xvf libzip-1.3.2.tar.gz
 cd libzip-1.3.2
 ./configure
