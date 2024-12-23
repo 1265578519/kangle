f84c53313eb7cee6961d38536a3a76907a5daca6
小樱 <1265578519@qq.com>
2024-12-23 23:20:24 +0800

    更新php7插件三个版本
    更新php7插件三个版本
    7133
    7234
    7333
    稍后编译上传极速安装

---
+++ php/7.1/7133/completed/tpl_php7133.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum -y install bzip2-devel libxml2-devel curl-devel db4-devel libjpeg-devel libpng-devel freetype-devel pcre-devel zlib-devel sqlite-devel libmcrypt-devel unzip bzip2
+yum -y install mhash-devel openssl-devel
+yum -y install libtool-ltdl libtool-ltdl-devel
+PREFIX="/vhs/kangle/ext"
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/7.1/7133/completed/tpl_php7133.tar.bz2 -O tpl_php7133.tar.bz2
+tar xjf tpl_php7133.tar.bz2
+mv tpl_php7133 $PREFIX
+rm -rf /tmp/*
+/vhs/kangle/bin/kangle -r
---
+++ php/7.3/7333/completed/tpl_php7333.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+yum -y install bzip2-devel libxml2-devel curl-devel db4-devel libjpeg-devel libpng-devel freetype-devel pcre-devel zlib-devel sqlite-devel libmcrypt-devel unzip bzip2
+yum -y install mhash-devel openssl-devel
+yum -y install libtool-ltdl libtool-ltdl-devel
+yum -y remove libzip-devel
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/7.3/730/libzip-1.3.2.tar.gz -O libzip-1.3.2.tar.gz
+tar xvf libzip-1.3.2.tar.gz
+cd libzip-1.3.2
+./configure
+make -j 4
+make install
+cd ..
+mv -f /usr/lib64/libzip.so.5 /usr/lib64/libzip.so.5.bak
+ln -s /usr/local/lib/libzip.so.5 /usr/lib64/libzip.so.5
+PREFIX="/vhs/kangle/ext"
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/7.3/7333/completed/tpl_php7333.tar.bz2 -O tpl_php7333.tar.bz2
+tar xjf tpl_php7333.tar.bz2
+mv tpl_php7333 $PREFIX
+rm -rf /tmp/*
+/vhs/kangle/bin/kangle -r
