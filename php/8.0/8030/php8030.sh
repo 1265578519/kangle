17459780e6676553aca8afbf167039e13f34673a
小樱 <1265578519@qq.com>
2024-12-25 04:29:15 +0800

    更新

--- php/8.0/8030/php8030.sh
+++ php/8.0/8030/php8030.sh
@@ -109,9 +109,9 @@ make -j 4
 make install
 cd ..
 #install memcache
-wget -c http://github.itzmx.com/1265578519/kangle/master/php/8.0/8030/php-memcache-4.0.4-dev.zip
-unzip -o php-memcache-4.0.4-dev.zip
-cd php-memcache-4.0.4-dev
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/8.0/8030/php-memcache-8.2.zip
+unzip -o php-memcache-8.2.zip
+cd php-memcache-8.2
 /vhs/kangle/ext/tpl_php8030/bin/phpize
 ./configure --with-php-config=/vhs/kangle/ext/tpl_php8030/bin/php-config
 make -j 4
