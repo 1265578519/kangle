3bbd6a0e981c7e2a2483122187c89d6bcb72f81e
小樱 <1265578519@qq.com>
2024-12-26 02:01:21 +0800

    测试8.1
    测试8.1

---
+++ php/8.1/8131/completed/tpl_php8131.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+yum -y install bzip2-devel libxml2-devel curl-devel db4-devel libjpeg-devel libpng-devel freetype-devel pcre-devel zlib-devel sqlite-devel libmcrypt-devel unzip bzip2
+yum -y install mhash-devel openssl-devel
+yum -y install libtool-ltdl libtool-ltdl-devel
+yum -y remove libzip-devel
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/8.1/8131/libzip-1.3.2.tar.gz -O libzip-1.3.2.tar.gz
+tar xvf libzip-1.3.2.tar.gz
+cd libzip-1.3.2
+./configure
+make -j 4
+make install
+cd ..
+mv -f /usr/lib64/libzip.so.5 /usr/lib64/libzip.so.5.bak
+ln -s /usr/local/lib/libzip.so.5 /usr/lib64/libzip.so.5
+ln -s /usr/local/lib/pkgconfig/libzip.pc /usr/lib64/pkgconfig/libzip.pc
+echo '/usr/local/lib' > /etc/ld.so.conf.d/libzip.conf
+ldconfig -v
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/8.1/8131/libxml2-2.9.0.tar.gz
+tar -zxvf libxml2-2.9.0.tar.gz
+cd libxml2-2.9.0
+./configure
+make -j 4
+make install
+cd ..
+rm -rf /usr/bin/xml2-config.OFF
+mv /usr/bin/xml2-config /usr/bin/xml2-config.OFF
+ln -s /usr/local/lib/pkgconfig/libxml-2.0.pc /usr/lib64/pkgconfig/libxml-2.0.pc
+ln -s /usr/local/bin/xml2-config /usr/bin/xml2-config
+ldconfig -v
+PREFIX="/vhs/kangle/ext"
+wget -c http://github.itzmx.com/1265578519/kangle/master/php/8.1/8131/completed/tpl_php8131.tar.bz2 -O tpl_php8131.tar.bz2
+tar xjf tpl_php8131.tar.bz2
+mv tpl_php8131 $PREFIX
+rm -rf /tmp/*
+/vhs/kangle/bin/kangle -r
